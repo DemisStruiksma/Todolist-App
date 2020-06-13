20c14410b1cf08661392b6b1d0240653f284ef30
Demis Struiksma <demis@thenextweb.com>
2020-06-13 16:12:45 +0200

    Add selectors

---
+++ scripts/todo.js
@@ -0,0 +1,4 @@
+const todoList = document.querySelector('#todo-list');
+const input = document.querySelector('.input');
+const addBtn = document.querySelector('.add-btn');
+const clearBtn = document.querySelector('.clear-all-btn');
